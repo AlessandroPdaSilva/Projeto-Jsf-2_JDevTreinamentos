43dc6806df82463320cf0f007f4ad7ba1d376bc9
Alessandro <alessandropds55@gmail.com>
2022-08-05 22:49:01 -0300

    Atualizando formulario

--- jsf-projeto2/src/main/java/jsf/PessoaBean.java
+++ jsf-projeto2/src/main/java/jsf/PessoaBean.java
@@ -1,8 +1,13 @@
 package jsf;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -17,10 +22,46 @@ public class PessoaBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Pessoa pessoa = new Pessoa();
+	private List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
 	
+	@Inject
 	private DaoGenerico<Pessoa> daoPessoa;
 
+	public String salvar() {
+		pessoa = daoPessoa.editar(pessoa);
+		lista();
+		
+		FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage("salvo com sucesso!!"));
+		return "";
+	}
 	
+	public String editar() {
+		
+		lista();
+		return "";
+	}
+	
+	public String novo() {
+		pessoa = new Pessoa();
+		return "";
+	}
+	
+	//DELETAR
+	public String deletar(){
+		daoPessoa.deletar(pessoa);
+		novo();
+		lista();
+		
+		FacesContext.getCurrentInstance().addMessage("msg", new FacesMessage("deletado com sucesso!!"));
+		return "";
+	}
+	
+	//LISTAR
+	@PostConstruct
+	public void lista(){
+		listaPessoa = daoPessoa.listar(Pessoa.class,10);
+	}
+
 	
 	// GET E SET
 	public Pessoa getPessoa() {
@@ -29,6 +70,14 @@ public class PessoaBean implements Serializable{
 
 	public void setPessoa(Pessoa p) {
 		this.pessoa = p;
+	}
+
+	public List<Pessoa> getListaPessoa() {
+		return listaPessoa;
+	}
+
+	public void setListaPessoa(List<Pessoa> listaPessoa) {
+		this.listaPessoa = listaPessoa;
 	}
 	
 	
